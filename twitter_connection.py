193ba16710a3575130a2e6e8f36e278f3e958343
santoshi <kalaskars1996@gmail.com>
2021-03-04 09:49:45 +0530

    Added : twiiter connection & tweets Extraction..!

---
+++ twitter_connection.py
@@ -0,0 +1,65 @@
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+import socket
+import json
+
+consumer_key='G9H8AwQRbrKShZbK6Q29fC4hb'
+consumer_secret='L4r36DhGsbl2H5rZ5u7JoGuEjlfGLWxPutwib2SPOd7D42K78t'
+access_token ='3014372628-D4TGlcAwoqmODLl3iKgQduixZMyOIuF0Wf73Bm2'
+access_secret='lhYA0n2I3TTdXOmg6j967v6pJ3bWZ2Ba08Sx4fKweGcBt'
+
+class TweetsListener(StreamListener):
+  # tweet object listens for the tweets
+  def __init__(self, csocket):
+    self.client_socket = csocket
+  def on_data(self, data):
+    try:  
+      msg = json.loads( data )
+      print("new message")
+      # if tweet is longer than 140 characters
+      if "extended_tweet" in msg:
+        # add at the end of each tweet "t_end" 
+        self.client_socket\
+            .send(str(msg['extended_tweet']['full_text']+"t_end")\
+            .encode('utf-8'))         
+        print(msg['extended_tweet']['full_text'])
+      else:
+        # add at the end of each tweet "t_end" 
+        self.client_socket\
+            .send(str(msg['text']+"t_end")\
+            .encode('utf-8'))
+        print(msg['text'])
+      return True
+    except BaseException as e:
+        print("Error on_data: %s" % str(e))
+    return True
+  def on_error(self, status):
+    print(status)
+    return True
+
+def sendData(c_socket, keyword):
+  print('start sending data from Twitter to socket')
+  # authentication based on the credentials
+  auth = OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_secret)
+  # start sending data from the Streaming API 
+  twitter_stream = Stream(auth, TweetsListener(c_socket))
+  twitter_stream.filter(track = keyword, languages=["en"])
+
+if __name__ == "__main__":
+    # server (local machine) creates listening socket
+    s = socket.socket()
+    host = "127.0.0.1"    
+    port = 9999
+    s.bind((host, port))
+    print('socket is ready')
+    # server (local machine) listens for connections
+    s.listen(4)
+    print('socket is listening')
+    # return the socket and the address on the other side of the connection (client side)
+    c_socket, addr = s.accept()
+    print("Received request from: " + str(addr))
+    # select here the keyword for the tweet data
+    sendData(c_socket, keyword = ['piano'])
